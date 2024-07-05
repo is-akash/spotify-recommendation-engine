cac31651172f7fa893ebd2493616ada63af0a1e3
skies-codes <akashdebnathwd@gmail.com>
2024-07-05 15:41:37 +0530

    added new dependencies and setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss';
+import {
+  Router,
+  Route,
+  redirect,
+  useLocation
+} from "react-router-dom";
+import { Provider } from 'react-redux';
+import { rootReducer } from './store/index';
+
+
+// import { Home } from './components/home/home';
+// import { About } from './components/about/about';
+// import { login } from './store/authentication/actions';
+// import { isNull, isNullOrUndefined } from 'util';
+// import Recommender from './components/recommender/recommender';
+
+// TODO: IMPORT store
 
 function App() {
-  const [count, setCount] = useState(0)
+  const accessToken = localStorage.getItem('access_token');
+  const validUntil = localStorage.getItem('valid_until');
+
+  if (accessToken && validUntil) {
+    store.dispatch(login(accessToken, parseInt(validUntil)));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {isAuthenticated() ? <Redirect to="/recommender" /> : <Home />}}
+          </Route>
+
+          <Authenticate path="/authenticate"></Authenticate>
+
+          <PrivateRoute exact path="/recommender">
+            <Recommender />
+          </PrivateRoute>
+
+          <Route exact path="/about">
+            <About />
+          </Route>
+
+          <Route path="*">
+            <Redirect to="/"/>
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
+  );
 }
 
-export default App
+function PrivateRoute({ children, ...rest }: any) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated() ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function isAuthenticated() {
+  const state = store.getState().authentication;
+
+  if (isNull(state.access_token) || isNull(state.valid_until)) {
+    return false;
+  }
+
+  return state.valid_until > Date.now();
+}
+
+function Authenticate({ children, ...rest }: any) {
+  let query = new URLSearchParams(useLocation().hash);;
+
+  const accessToken = query.get('#access_token')!;
+
+  // User denied access
+  if(isNullOrUndefined(accessToken)) {
+    return <Redirect to="/"/>
+  }
+
+  const validUntil = Date.now() + parseInt(query.get('expires_in')!) * 1000;
+  store.dispatch(login(accessToken, validUntil));
+
+  localStorage.setItem('access_token', accessToken);
+  localStorage.setItem('valid_until', validUntil.toString());
+
+  return <Redirect to="/recommender"/>;
+}
+
+export default App;
